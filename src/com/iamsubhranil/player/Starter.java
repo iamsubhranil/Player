0cbe34a425f1414ef9183212df2e7b89e2b390b4
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-01-18 22:07:37 +0530

    Added styles

---
+++ src/com/iamsubhranil/player/Starter.java
@@ -0,0 +1,24 @@
+/*
+    Created By : iamsubhranil
+    Date : 18/1/17
+    Time : 9:49 PM
+    Package : com.iamsubhranil.player
+    Project : Player
+*/
+package com.iamsubhranil.player;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        System.setProperty("prism.lcdtext", "false");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
